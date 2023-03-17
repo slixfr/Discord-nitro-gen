d0264e3a165d427e8835d5bf19d20cd1e04cbc09
s0l <124410755+s0l69@users.noreply.github.com>
2023-03-17 16:47:10 +1100

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import string
 import os
 import time
 LICNECE = """
-Copyright (c) 2021 s0l#4395 
+Copyright (c) 2021 s0l#6969
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
@@ -77,7 +77,7 @@ class NitroGen:  # Initialise the class
 ╚═════╝░░╚════╝░╚══════╝""")  # Print the title card
         time.sleep(2)  # Wait a few seconds
         # Print who developed the code
-        self.slowType("Made by: s0l#4395 && ! StravZ#1239", .02)
+        self.slowType("Made by: s0l#6969 && ! StravZ#1239", .02)
         time.sleep(1)  # Wait a little more
         # Print the first question
         self.slowType(
